fd65a7973711595af032084c1bf309864f6ed8eb
Iaroslav <jaro_r@mail.ru>
2022-12-20 17:54:01 +0700

    Added loading from and saving to binary files for gridTile class.

--- src/game/GridTile.cpp
+++ src/game/GridTile.cpp
@@ -18,3 +18,28 @@ void GridTile::render(Renderer* renderer){
     SDL_Rect tileRect = {x, y, TILESIZE, TILESIZE};
     renderer->renderTexture(gridTileTextureType, &tileRect);
 }
+
+void GridTile::saveToBinaryFile(ostream& outpustStream){
+
+    if (DEBUG_CONSOLE_OUTPUT_ON && !SAVING_LEVELS_ON)
+        cout << "Saving levels is turned off. Saving grid tile stopped." << endl;
+
+    if (!SAVING_LEVELS_ON)
+        return;
+    
+    // save grid tile texture type
+    outpustStream.write((char*)&gridTileTextureType, sizeof(gridTileTextureType));
+}
+
+void GridTile::loadFromBinaryFile(istream& inputStream){
+
+    if (DEBUG_CONSOLE_OUTPUT_ON && !LOADING_LEVELS_ON)
+        cout << "Loading levels is turned off. Loading grid tile stopped." << endl;
+
+    if (!LOADING_LEVELS_ON)
+        return;
+
+    // load grid tile texture type
+    inputStream.read((char*)&gridTileTextureType, sizeof(gridTileTextureType));
+
+}
--- src/game/GridTile.h
+++ src/game/GridTile.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <fstream>
+#include <iostream>
 
 #include "SDL2/SDL.h"
 
@@ -8,11 +10,21 @@
 #include "../Coords.h"
 #include "../Renderer.h"
 
+using std::ostream;
+using std::istream;
+using std::cout;
+using std::endl;
+
 /*class for tile of the grid*/
 class GridTile{
     public:
     GridTile(TexturesEnumeration gridTileTextureType, Coords coords);
     ~GridTile();
+
+        // file must be opened in binary mode. Using with other streams is not recommended(unknown result).
+    void saveToBinaryFile(ostream& outpustStream);
+    // file must be opened in binary mode. Using with other streams is not recommended(unknown result).
+    void loadFromBinaryFile(istream& inputStream);
 
     void render(Renderer* renderer);
     
