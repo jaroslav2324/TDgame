66dcd27798e15a311d3ab3ae3827ce1d59ad8a86
jaroslav2324 <jaro_r@mail.ru>
2023-08-22 07:11:38 +0300

    Changed cmake file and deleted build folder

--- build/libs/SDL/include/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-release-2.26.0-112-gc542aef60 (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-release-2.26.0-112-gc542aef60"
-#endif
